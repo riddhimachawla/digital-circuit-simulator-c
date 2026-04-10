c606ed8d9e242c156fd700f651db908eca54d7c7
riddhimachawla <riddhimachawla006@gmail.com>
2026-04-10 22:48:58 +0530

    Added truth table generation for OR gate using bit manipulation

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main() {
 
     int A, B;
 
-    printf("Truth Table for AND Gate\n");
-    printf("A B | Output\n");
+    printf("Truth Table for AND and OR Gates\n");
+    printf("A B | AND | OR\n");
     printf("-------------\n");
 
     for (int i = 0; i < 4; i++) {
@@ -13,9 +13,11 @@ int main() {
         A = (i >> 1) & 1;  // Extract first bit
         B = i & 1;         // Extract second bit
 
-        int result = A && B;
+        int and_result = A && B;
+        int or_result = A || B;
 
-        printf("%d %d |   %d\n", A, B, result);
+
+        printf("%d %d |   %d  |  %d\n", A, B, and_result, or_result);
     }
 
     return 0;
